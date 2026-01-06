9e953fdcdfeda73313057ebc834b93011f1d9bd9
isajisa <isabel.j.s.2004@gmail.com>
2026-01-06 21:32:12 +0100

    fotos editadas y pagina terminada no funcional

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+document.addEventListener("DOMContentLoaded", () => {
+    const modal = document.getElementById("exampleModal");
+    const video = document.getElementById("tutorialVideo");
+
+    modal.addEventListener("hidden.bs.modal", () => {
+        video.pause();
+        video.currentTime = 0;
+    });
+});
